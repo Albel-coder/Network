ab89b4014256432beb87895fd47966cd4071b3f5
Aleksey <alekseybel2505@gmail.com>
2024-11-03 19:46:00 +0300

    implemented an activation function

--- ActivateFunction.cpp
+++ ActivateFunction.cpp
@@ -1,1 +1,82 @@
 #include "ActivateFunction.h"
+void ActivateFunction::set()
+{
+	std::cout << "Set activate function\n0 - sigmoid \n1 - ReLU \n2 - th(x) \n";
+	short tmp;
+	std::cin >> tmp;
+	switch (tmp)
+	{
+	case sigmoid:
+		actFunc = sigmoid;
+		break;
+	case ReLU:
+		actFunc = ReLU;
+		break;
+	case thx:
+		actFunc = thx;
+		break;
+	default:
+		throw std::runtime_error("Error read activate function");
+		break;
+	}
+}
+void ActivateFunction::use(double* value, unsigned short n)
+{
+	switch (actFunc)
+	{
+	case activate_function::sigmoid:
+		for (int i = 0; i < n; i++) { value[i] = 1 / (1 + exp(-value[i])); }
+		break;
+	case activate_function::ReLU:
+		for (int i = 0; i < n; i++)
+		{
+			if (value[i] < 0) { value[i] *= 0.01; }
+			else if (value[i] > 1) { value[i] = 1.0 + 0.01 * (value[i] - 1); }
+		}
+	case activate_function::thx:
+		for (int i = 0; i < n; i++)
+		{
+			if (value[i] < 0) 
+			{
+				value[i] = 0.01 * (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
+			}
+			else 
+			{
+				value[i] = (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
+			}
+		}
+		break;
+	default:
+		throw std::runtime_error("Error activate function\n");
+		break;
+	}
+}
+void ActivateFunction::Derivate(double* value, unsigned short n)
+{
+	switch (actFunc)
+	{
+	case activate_function::sigmoid:
+		for (int i = 0; i < n; i++) 
+		{
+			value[i] = value[i] * (1 - value[i]);
+		}
+		break;
+	case activate_function::ReLU:
+		for (int i = 0; i < n; i++)
+		{
+			if (value[i] < 0 || value[i] > 1) { value[i] = 0.01; }
+			else { value[i] = 1; }
+		}
+		break;
+	case activate_function::thx:
+		for (int i = 0; i < n; i++)
+		{
+			if (value[i] < 0) { value[i] = 0.01 * (1 - value[i] * value[i]); }
+			else { value[i] = 1 - value[i] * value[i]; }
+		}
+		break;
+	default:
+		throw std::runtime_error("Error activate function derivate\n");
+		break;
+	}
+}
--- ActivateFunction.h
+++ ActivateFunction.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <iostream>
+enum activate_function {sigmoid = 1, ReLU, thx };
+
 class ActivateFunction
 {
+	activate_function actFunc;
+public:
+	void set();
+	void use(double* value, unsigned short n);
+	void Derivate(double* value, unsigned short n);
 };
 
