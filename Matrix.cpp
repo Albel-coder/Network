5707fe0d3ebc92fba401a8b837f01bbfe198b4f3
Aleksey <alekseybel2505@gmail.com>
2026-02-23 00:52:10 +0300

    finally: rebase project

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,99 +3,99 @@
 #include <stdexcept>
 
 Matrix::Matrix(size_type rows, size_type cols, value_type init)
-    : data(rows* cols, init), rows_(rows), cols_(cols) {
-    if (rows == 0 || cols == 0) {
-        data.clear();
-        rows_ = cols_ = 0;
-    }
+	: data(rows * cols, init), rows_(rows), cols_(cols) {
+	
+	if (rows == 0 || cols == 0) {
+		data.clear();
+		rows_ = cols_ = 0;
+	}
 }
 
-Matrix::value_type& Matrix::operator()(size_type i, size_type j)
-{
-    if (i >= rows_ || j >= cols_) 
-        throw std::out_of_range("Matrix index out of range");
+Matrix::value_type& Matrix::operator()(size_type i, size_type j) {
+	if (i >= rows_ || j >= cols_) 
+		throw std::out_of_range("Matrix index out of range");
 
-    return data[i * cols_ + j];
+	return data[i * cols_ + j];
 }
 
 const Matrix::value_type& Matrix::operator()(size_type i, size_type j) const {
-    return data[i * cols_ + j];
+	return data[i * cols_ + j];
 }
 
 void Matrix::random() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    std::uniform_real_distribution<value_type> dis(0.0, 1.0);
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::uniform_real_distribution<value_type> dis(0.0, 1.0);
 
-    for (auto& val : data) {
-        val = dis(gen);
-    }
+	for (auto& value : data) {
+		value = dis(gen);
+	}
 }
 
-std::vector<Matrix::value_type> Matrix::multiply(const std::vector<value_type>& vec) const {
-    if (cols_ != vec.size()) {
-        throw std::runtime_error("Matrix multiply: dimension mismatch");
-    }
-    std::vector<value_type> result(rows_, value_type{});
+std::vector<Matrix::value_type> Matrix::multiply(const std::vector<value_type>& vector) const {
+	if (cols_ != vector.size()) {
+		throw std::runtime_error("Matrix multiply: dimension mismatch");
+	}
+	std::vector<value_type> result(rows_, value_type{});
+	for (size_type i = 0; i < rows_; ++i) {
+		value_type sum = 0;
 
-    for (size_type i = 0; i < rows_; ++i) {
-        value_type sum = 0;
+		for (size_type j = 0; j < cols_; ++j) {
+			sum += data[i * cols_ + j] * vector[j];
+		}
 
-        for (size_type j = 0; j < cols_; ++j) {
-            sum += data[i * cols_ + j] * vec[j];
-        }
-        result[i] = sum;
-    }
+		result[i] = sum;
+	}
 
-    return result;
+	return result;
 }
 
-std::vector<Matrix::value_type> Matrix::multiply_transposed(const std::vector<value_type>& vec) const {
-    if (rows_ != vec.size()) {
-        throw std::runtime_error("Matrix multiply_transposed: dimension mismatch");
-    }
-    std::vector<value_type> result(cols_, value_type{});
+std::vector<Matrix::value_type> Matrix::multiplyTransposed(const std::vector<value_type>& vector) const {
+	if (cols_ != vector.size()) {
+		throw std::runtime_error("Matrix multiply: dimension mismatch");
+	}
+	std::vector<value_type> result(rows_, value_type{});
+	for (size_type i = 0; i < rows_; ++i) {
+		value_type sum = 0;
 
-    for (size_type j = 0; j < cols_; ++j) {
-        value_type sum = 0;
+		for (size_type j = 0; j < cols_; ++j) {
+			sum += data[i * cols_ + j] + vector[j];
+		}
 
-        for (size_type i = 0; i < rows_; ++i) {
-            sum += data[i * cols_ + j] * vec[i];
-        }
-        result[j] = sum;
-    }
+		result[i] = sum;
+	}
 
-    return result;
+	return result;
 }
 
-void Matrix::add_to_vector(std::vector<value_type>& a, const std::vector<value_type>& b) {
-    if (a.size() != b.size()) {
-        throw std::runtime_error("add_to_vector: size mismatch");
-    }
+void Matrix::addToVector(std::vector<value_type>& a, const std::vector<value_type>& b) {
+	if (a.size() != b.size()) {
+		throw std::runtime_error("addToVector: size mismatch");
+	}
 
-    for (size_type i = 0; i < a.size(); ++i) {
-        a[i] += b[i];
-    }
+	for (size_type i = 0; i < a.size(); i++) {
+		a[i] += b[i];
+	}
 }
 
 std::ostream& operator<<(std::ostream& os, const Matrix& m) {
-    for (Matrix::size_type i = 0; i < m.rows_; ++i) {
-        for (Matrix::size_type j = 0; j < m.cols_; ++j) {
-            os << m.data[i * m.cols_ + j] << ' ';
-        }
-        os << '\n';
-    }
+	for (Matrix::size_type i = 0; i < m.rows_; ++i) {
+		for (Matrix::size_type j = 0; j < m.cols_; ++j) {
+			os << m.data[i * m.cols_ + j] << ' ';
+		}
+		os << '\n';
+	}
 
-    return os;
+	return os;
 }
 
 std::istream& operator>>(std::istream& is, Matrix& m) {
-    for (auto& val : m.data) {
-        is >> val;
-        if (!is) {
-            break;
-        }
-    }
+	for (auto& value : m.data) {
+		is >> value;
+		if (!is) {
+			break;
+		}
+	}
 
-    return is;
+	return is;
 }
--- Matrix.h
+++ Matrix.h
@@ -3,39 +3,40 @@
 #include <vector>
 #include <cstddef>
 
-class Matrix {
+class Matrix
+{
 public:
-    using value_type = double;
-    using size_type = std::size_t;
+	using value_type = double;
+	using size_type = std::size_t;
 
 private:
-    std::vector<value_type> data;
-    size_type rows_ = 0;
-    size_type cols_ = 0;
+	std::vector<value_type> data;
+	size_type rows_ = 0;
+	size_type cols_ = 0;
 
 public:
-    Matrix() = default;
-    Matrix(size_type rows, size_type cols, value_type init = value_type{});
-    Matrix(const Matrix&) = default;
-    Matrix(Matrix&&) = default;
+	Matrix() = default;
+	Matrix(size_type rows, size_type cols, value_type init = value_type{});
+	Matrix(const Matrix&) = default;
+	Matrix(Matrix&&) = default;
 
-    Matrix& operator=(const Matrix&) = default;
-    Matrix& operator=(Matrix&&) = default;
+	Matrix& operator=(const Matrix&) = default;
+	Matrix& operator=(Matrix&&) = default;
 
-    value_type& operator()(size_type i, size_type j);
-    const value_type& operator()(size_type i, size_type j) const;
+	value_type& operator()(size_type i, size_type j);
+	const value_type& operator()(size_type i, size_type j) const;
 
-    size_type rows() const noexcept { return rows_; }
-    size_type cols() const noexcept { return cols_; }
+	size_type rows() const noexcept { return rows_; }
+	size_type cols() const noexcept { return cols_; }
 
-    void random();
+	void random();
 
-    std::vector<value_type> multiply(const std::vector<value_type>& vec) const;
+	std::vector<value_type> multiply(const std::vector<value_type>& vector) const;
 
-    std::vector<value_type> multiply_transposed(const std::vector<value_type>& vec) const;
+	std::vector<value_type> multiplyTransposed(const std::vector<value_type>& vector) const;
 
-    static void add_to_vector(std::vector<value_type>& a, const std::vector<value_type>& b);
+	static void addToVector(std::vector<value_type>& a, const std::vector<value_type>& b);
 
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
-    friend std::istream& operator>>(std::istream& is, Matrix& m);
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+	friend std::istream& operator>>(std::istream& is, Matrix& m);
 };
