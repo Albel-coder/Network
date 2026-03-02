b2dcf83b3d85d7421b90fc0cbf70159916c32c7d
Aleksey <alekseybel2505@gmail.com>
2026-03-02 20:45:43 +0300

    Little update in Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -5,38 +5,34 @@
 
 class Matrix
 {
-public:
-	using value_type = double;
-	using size_type = std::size_t;
-
 private:
-	std::vector<value_type> data;
-	size_type rows_ = 0;
-	size_type cols_ = 0;
+	std::vector<double> data;
+	std::size_t rows = 0;
+	std::size_t cols = 0;
 
 public:
 	Matrix() = default;
-	Matrix(size_type rows, size_type cols, value_type init = value_type{});
+	Matrix(std::size_t rows, std::size_t cols, double init = double{});
 	Matrix(const Matrix&) = default;
 	Matrix(Matrix&&) = default;
 
 	Matrix& operator=(const Matrix&) = default;
 	Matrix& operator=(Matrix&&) = default;
 
-	value_type& operator()(size_type i, size_type j);
-	const value_type& operator()(size_type i, size_type j) const;
+	double& operator()(std::size_t i, std::size_t j);
+	const double& operator()(std::size_t i, std::size_t j) const;
 
-	size_type rows() const noexcept { return rows_; }
-	size_type cols() const noexcept { return cols_; }
+	std::size_t rows() const noexcept { return rows; }
+	std::size_t cols() const noexcept { return cols; }
 
 	void random();
 
-	std::vector<value_type> multiply(const std::vector<value_type>& vector) const;
+	std::vector<double> multiply(const std::vector<double>& vector) const;
 
-	std::vector<value_type> multiplyTransposed(const std::vector<value_type>& vector) const;
+	std::vector<double> multiplyTransposed(const std::vector<double>& vector) const;
 
-	static void addToVector(std::vector<value_type>& a, const std::vector<value_type>& b);
+	static void addToVector(std::vector<double>& firstVector, const std::vector<double>& secondVector);
 
-	friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
-	friend std::istream& operator>>(std::istream& is, Matrix& m);
+	friend std::ostream& operator<<(std::ostream& output, const Matrix& matrix);
+	friend std::istream& operator>>(std::istream& input, Matrix& matrix);
 };
